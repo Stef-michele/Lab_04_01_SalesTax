fa34aaa11bad698b5b85046253ab54ae3dd0cd2f
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-01 15:58:55 -0500

    Finished tax

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {   double purPrice = 555.00;
         double salTax = 0.05;
         double taxTotal = purPrice * salTax;
         double totPrice = purPrice + taxTotal;
         System.out.println( "Your sales tax is $" + taxTotal + " and the total price including tax is $" + totPrice );
-
 
     }
 }
