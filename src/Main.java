833a402d19a2686d25def1777df3bc0cf39f643a
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-04 00:07:39 -0500

    Finished corrected

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main
 {
     public static void main(String[] args)
-    {   double purPrice = 555.00;
+    {   //declaring variables
+        double purPrice = 555.00;
         double salTax = 0.05;
-        double taxTotal = purPrice * salTax;
-        double totPrice = purPrice + taxTotal;
+        double taxTotal = 0.0;
+        double totPrice = 0.0;
+        //calculating tax amount and total price
+        taxTotal = purPrice * salTax;
+        totPrice = purPrice + taxTotal;
+        //output tax amount and total price
         System.out.println( "Your sales tax is $" + taxTotal + " and the total price including tax is $" + totPrice );
 
     }
